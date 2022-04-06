cb72410716a8d7a4c96145c93d987b69eac7280d
Mariana <a01702593@itesm.mx>
2022-04-06 11:57:12 -0500

    add react-router-dom and MVC structure

--- capullo-app/src/App.jsx
+++ capullo-app/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './components/login';
+
 function App() {
-  return <p>Hola mundo</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ capullo-app/src/components/login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return (<h1>Welcome to the world of Geeks!</h1>);
+}
+
+export default Login;
