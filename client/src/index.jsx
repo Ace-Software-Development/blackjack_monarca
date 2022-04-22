391021633175d02ec11bbeefe0c84b5d3e3545e2
Diego <A01552594@itesm.mx>
2022-04-21 19:22:54 -0500

    add rechazado interface

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Inicio from './components/Inicio';
+//  import Rechazado from './components/Rechazado';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
