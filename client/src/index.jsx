3810595e828781c1d71d13d1d648600e141b3755
Diego <A01552594@itesm.mx>
2022-04-26 19:19:31 -0500

    add login interface

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Inicio from './components/Inicio';
+import './styleLogin.css';
+//  import Inicio from './components/Inicio';
+import Login from './components/login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root'),
 );
