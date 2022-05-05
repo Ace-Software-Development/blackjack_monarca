45d08509bb45e86d657ee9d381a1d4cb7263290b
MarianaS8a <58479423+MarianaS8a@users.noreply.github.com>
2022-05-04 23:42:16 -0500

    Update dashboard.js

--- server/parse/dashboard.js
+++ server/parse/dashboard.js
@@ -1,5 +1,12 @@
+/** * @author Mariana Soto Ochoa <a01702593@itesm.mx>
+ * Parse dasboard for database management
+ * @version 1.0
+ * @since 1.0
+ */
+
 const ParseDashboard = require('parse-dashboard');
 
+//Creates dashboard
 const dashboard = new ParseDashboard({
     "apps": [{
         "serverURL": process.env.SERVER_URL,
@@ -14,6 +21,7 @@ const dashboard = new ParseDashboard({
     }]
 }, { allowInsecureHTTP: true })
 
+//Export dashboard to other files
 module.exports = {
   dashboard: dashboard,
   url: '/dashboard'
