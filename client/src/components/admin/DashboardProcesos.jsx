c25d8031a6079167616baac6d67a908df9eb3b78
A01705746 <65366693+A01705746@users.noreply.github.com>
2022-06-06 17:03:59 -0500

    Add graphic process complete

--- client/src/components/admin/DashboardProcesos.jsx
+++ client/src/components/admin/DashboardProcesos.jsx
@@ -46,6 +46,301 @@ function DashboardProcesos() {
         return ('No tienes permisos');
     }
 
+    const [incidents, setIncidents] = useState([]);
+    /**
+     * getAllIncidents
+     * @description Get all incidents confirmed
+     * */
+    async function getAllIncidents() {
+        const response = await fetch('http://localhost:8888/procesos/allIncidents/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const incidentesList = await response.json();
+        setIncidents(incidentesList.data);
+    }
+
+    const [disks, setDisks] = useState([]);
+    /**
+     * getAllDisks
+     * @description Get all history of disks
+     * */
+    async function getAllDisks() {
+        const response = await fetch('http://localhost:8888/procesos/allDisks/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const diskList = await response.json();
+        setDisks(diskList.data);
+    }
+
+    const [esmerilados, setEsmerilados] = useState([]);
+    /**
+     * getAllEsmerilados
+     * @description Get all pieces confirmed in esmerilado
+     * */
+    async function getAllEsmerilados() {
+        const response = await fetch('http://localhost:8888/procesos/allEsmerilados/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const esmeriladosList = await response.json();
+        setEsmerilados(esmeriladosList.data);
+    }
+
+    const [esmeriladosIncident, setEsmeriladosIncident] = useState([]);
+    /**
+     * getEsmeriladosIncident
+     * @description Get all pieces sended as incident and confirmed in esmerilado
+     * */
+    async function getEsmeriladosIncident() {
+        const response = await fetch('http://localhost:8888/procesos/EsmeriladosIncident/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const esmeriladosIncidentsList = await response.json();
+        setEsmeriladosIncident(esmeriladosIncidentsList.data);
+    }
+
+    const [pulidos, setPulidos] = useState([]);
+    /**
+     * getAllPulidos
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getAllPulidos() {
+        const response = await fetch('http://localhost:8888/procesos/allPulidos/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const pulidosList = await response.json();
+        setPulidos(pulidosList.data);
+    }
+
+    const [pulidosIncident, setPulidosIncident] = useState([]);
+    /**
+     * getPulidosIncident
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getPulidosIncident() {
+        const response = await fetch('http://localhost:8888/procesos/PulidosIncident/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const pulidosIncidentList = await response.json();
+        setPulidosIncident(pulidosIncidentList.data);
+    }
+
+    const [remachados, setRemachados] = useState([]);
+    /**
+     * getAllRemachados
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getAllRemachados() {
+        const response = await fetch('http://localhost:8888/procesos/allRemachados/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const remachadosList = await response.json();
+        setRemachados(remachadosList.data);
+    }
+
+    const [remachadosIncident, setRemachadosIncident] = useState([]);
+    /**
+     * getRemachadosIncident
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getRemachadosIncident() {
+        const response = await fetch('http://localhost:8888/procesos/RemachadosIncident/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const remachadosIncidentList = await response.json();
+        setRemachadosIncident(remachadosIncidentList.data);
+    }
+
+    const [empaquetados, setEmpaquetados] = useState([]);
+    /**
+     * getAllEmpaquetados
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getAllEmpaquetados() {
+        const response = await fetch('http://localhost:8888/procesos/allEmpaquetados/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const empaquetadosList = await response.json();
+        setEmpaquetados(empaquetadosList.data);
+    }
+
+    const [empaquetadosInv, setEmpaquetadosInv] = useState([]);
+    /**
+     * getAllEmpaquetadosInv
+     * @description Get all pieces sended as incident and confirmed in pulido
+     * */
+    async function getAllEmpaquetadosInv() {
+        const response = await fetch('http://localhost:8888/procesos/EmpaquetadosInv/get');
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.customAlert(message);
+            return;
+        }
+
+        const empaquetadosInvList = await response.json();
+        setEmpaquetadosInv(empaquetadosInvList.data);
+    }
+
+    useEffect(() => {
+        getAllIncidents();
+        getAllDisks();
+        getAllEsmerilados();
+        getEsmeriladosIncident();
+        getAllPulidos();
+        getPulidosIncident();
+        getAllRemachados();
+        getRemachadosIncident();
+        getAllEmpaquetados();
+        getAllEmpaquetadosInv();
+    }, []);
+
+    function sumAll(query) {
+        let acum = 0;
+        for (let i = 0; i < query.length; i += 1) {
+            acum += query[i].number;
+        }
+        return acum;
+    }
+
+    function sumAllEmpaquetado(query) {
+        let acum = 0;
+        for (let i = 0; i < query.length; i += 1) {
+            acum += query[i].with_lid;
+            acum += query[i].withOut_lid;
+        }
+        return acum;
+    }
+
+    function sumAllDisks(query) {
+        let acum = 0;
+        for (let i = 0; i < query.length; i += 1) {
+            if (query[i].number < 0) {
+                acum += query[i].number;
+            }
+        }
+        return acum * -1;
+    }
+
+    const [totalEntradaIncidentes, setTotalEntradaIncidentes] = useState(0);
+    const [totalEntradaDiscos, setTotalEntradaDiscos] = useState(0);
+    const [totalEntradaEsmerilados, setTotalEntradaEsmerilados] = useState(0);
+    const [totalEntradaEsmeriladosIncident, setTotalEntradaEsmeriladosIncident] = useState(0);
+    const [totalEntradaPulidos, setTotalEntradaPulidos] = useState(0);
+    const [totalEntradaPulidosIncident, setTotalEntradaPulidosIncidents] = useState(0);
+    const [totalEntradaRemachados, setTotalEntradaRemachados] = useState(0);
+    const [totalEntradaRemachadosIncident, setTotalEntradaRemachadosIncident] = useState(0);
+    const [totalEntradaEmpaquetados, setTotalEntradaEmpaquetados] = useState(0);
+    const [totalEntradaEmpaquetadosInv, setTotalEntradaEmpaquetadosInv] = useState(0);
+
+    useEffect(() => {
+        setTotalEntradaIncidentes(sumAll(incidents));
+        setTotalEntradaDiscos(sumAllDisks(disks));
+        setTotalEntradaEsmerilados(sumAll(esmerilados));
+        setTotalEntradaEsmeriladosIncident(sumAll(esmeriladosIncident));
+        setTotalEntradaPulidos(sumAll(pulidos));
+        setTotalEntradaPulidosIncidents(sumAll(pulidosIncident));
+        setTotalEntradaRemachados(sumAll(remachados));
+        setTotalEntradaRemachadosIncident(sumAll(remachadosIncident));
+        setTotalEntradaEmpaquetados(sumAllEmpaquetado(empaquetados));
+        setTotalEntradaEmpaquetadosInv(sumAll(empaquetadosInv));
+    }, [
+        incidents,
+        disks,
+        esmerilados,
+        esmeriladosIncident,
+        pulidos,
+        pulidosIncident,
+        remachados,
+        remachadosIncident,
+        empaquetados,
+        empaquetadosInv,
+    ]);
+
+    function totalRechazado() {
+        return (totalEntradaIncidentes + totalEntradaDiscos) - totalEntradaEsmerilados;
+    }
+
+    function totalEsmerilado() {
+        return totalEntradaEsmerilados - (totalEntradaEsmeriladosIncident + totalEntradaPulidos);
+    }
+
+    function totalPulido() {
+        return totalEntradaPulidos - (totalEntradaPulidosIncident + totalEntradaRemachados);
+    }
+
+    function totalRemachado() {
+        return totalEntradaRemachados - (totalEntradaRemachadosIncident + totalEntradaEmpaquetados);
+    }
+
+    const [main, setMain] = useState([]);
+
+    useEffect(() => {
+        setMain([
+            {
+                name: 'Rechazado',
+                y: totalRechazado(),
+            },
+            {
+                name: 'Esmerilado',
+                y: totalEsmerilado(),
+            },
+            {
+                name: 'Pulido',
+                y: totalPulido(),
+            },
+            {
+                name: 'Remachado',
+                y: totalRemachado(),
+            },
+        ]);
+    }, [
+        totalEntradaIncidentes,
+        totalEntradaDiscos,
+        totalEntradaEsmerilados,
+        totalEntradaEsmeriladosIncident,
+        totalEntradaPulidos,
+        totalEntradaPulidosIncident,
+        totalEntradaRemachados,
+        totalEntradaRemachadosIncident,
+        totalEntradaEmpaquetados,
+        totalEntradaEmpaquetadosInv,
+    ]);
+
     const options = {
         title: {
             text: 'Piezas por área hoy',
@@ -80,24 +375,7 @@ function DashboardProcesos() {
                 type: 'column',
                 name: 'Productos',
                 id: 'Productos',
-                data: [
-                    {
-                        name: 'Rechazado',
-                        y: 1,
-                    },
-                    {
-                        name: 'Esmerilado',
-                        y: 4,
-                    },
-                    {
-                        name: 'Pulido',
-                        y: 3,
-                    },
-                    {
-                        name: 'Remachado',
-                        y: 8,
-                    },
-                ],
+                data: main,
             },
         ],
     };
