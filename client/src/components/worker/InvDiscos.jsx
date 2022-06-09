cd6323e955e38852e9498c96b75434fde950eae8
Leonardoalv201 <a01705998@itesm.mx>
2022-06-09 13:23:31 -0500

    fix permission to worker

--- client/src/components/worker/InvDiscos.jsx
+++ client/src/components/worker/InvDiscos.jsx
@@ -28,7 +28,6 @@ Mains.propTypes = {
 
 function InvDiscos() {
     const session = Cookies.get('sessionToken');
-    const admin = Cookies.get('is_admin');
     const [permission, setPermission] = useState([]);
 
     /**
@@ -48,8 +47,9 @@ function InvDiscos() {
     }
     useEffect(() => {
         getPermission();
-    }, [session, admin]);
-    if (admin === 'false' || !permission) {
+    }, []);
+
+    if (!permission) {
         return ('No tienes permisos');
     }
 
